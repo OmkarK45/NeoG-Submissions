23717fec9b29e5453598667395139e17a69fd7b7
Omkar Kulkarni <omkarak45@gmail.com>
2020-11-21 13:33:51 +0530

    Added basic code quiz

--- L0-W1-A1/quiz.js
+++ L0-W1-A1/quiz.js
@@ -1,3 +1,22 @@
-const readlineSync = require('readline-sync');
-const chalk = require('chalk');
+const readlineSync = require("readline-sync");
+const chalk = require("chalk");
 
+// Chalk Colors
+const colors = {
+  blue: chalk.bold.blue,
+  red: chalk.bold.red,
+  yellow: chalk.bold.yellow,
+  green: chalk.bold.green,
+  white: chalk.bold.white,
+};
+console.log(chalk.bgRed("⚡ Welcome to the GTA 5 Quiz ! ⚡"));
+
+// Player Intro
+const player = readlineSync.question(colors.green("Hey, What's your name ?\n"));
+
+console.log(chalk.bgRed.black(` Welcome ${player}, Let\'s play! `));
+
+// Rules of the game
+console.log(chalk.bgGreen.black(" Rules of this game are simple \n"));
+
+// Game Driver
