48ff0df4939f297f349acbc1772bff14e81c201f
Omkar Kulkarni <omkarak45@gmail.com>
2020-12-13 21:44:45 +0530

    Created Yoda Translator

---
+++ translator2/app.js
@@ -0,0 +1,44 @@
+const API_URL = "https://api.funtranslations.com/translate/yoda.json";
+const input = document.querySelector(".inputText");
+const btn = document.querySelector(".btn");
+const output = document.querySelector(".outputText");
+
+const handleError = (err) => {
+  btn.textContent = "Translate";
+  btn.disabled = false;
+  btn.classList.remove("disabledBtn");
+  console.log(err);
+  if (err.code === 429) {
+    alert("Please wait as API Rate Exceeded! Thank You :D");
+  } else {
+    alert("Some error occured ! Please refresh and try again.");
+  }
+};
+
+const translator = (inputText) => {
+  fetch(API_URL + "?text=" + inputText)
+    .then((res) => res.json())
+    .then((obj) => {
+      if (!obj.error) {
+        btn.textContent = "Translate";
+        btn.disabled = false;
+        btn.classList.remove("disabledBtn");
+        console.log(obj);
+        output.value = obj.contents.translated;
+      } else {
+        throw obj.error;
+      }
+    })
+    .catch((err) => {
+      handleError(err);
+    });
+};
+
+btn.addEventListener("click", () => {
+  let inputText = input.value;
+  btn.innerHTML = `<i class="fas fa-sync-alt"></i> Loading...`;
+  btn.disabled = true;
+  btn.classList.add("disabledBtn");
+  console.log(inputText);
+  translator(inputText);
+});
